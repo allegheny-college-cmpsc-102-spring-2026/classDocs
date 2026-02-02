c4abc4aa64e926ec3462897118dadd7baac24898
hangzhaogogogo <hzhao@allegheny.edu>
2026-02-02 09:26:50 -0500

    add week4

---
+++ lessons/week04/sandbox/hello_user/moreFun.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from rich.console import Console
+
+
+def giveMoreFun(myText: str) -> None:
+    """Function to send an extra part of the greeting"""
+    # create a console for rich text output
+    console = Console()
+    console.print(
+        f"\n\t This is the giveMoreFun() function sending a huge hello to : {myText}"
+    )
+
+
+# end of giveMoreFun()
---
+++ lessons/week04/sandbox/sciDemo/setupCommands.md
@@ -0,0 +1,13 @@
+python3 -m venv myvenv
+source myvenv/bin/activate
+pip install numpy
+pip install seaborn
+pip install scikit-learn
+# or try: pip install sklearn
+
+
+python3 main.py 
+
+# or
+
+python3 sciKitDemo.py 
